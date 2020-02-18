413dddfbff5684a7db621adda594caca80a9f490
keyaneealexandre <ka1974@nyu.edu>
2020-02-18 04:41:15 +0000

    problem1 solved

--- problem1.c
+++ problem1.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+int find (char dna[], char therons[]);
+int verify(char dna[]);
+
 int main() {
 
 	char dna[10000];
@@ -11,6 +14,10 @@ int main() {
 	printf("Enter the DNA sequecen: ");
 	//read up to 9999 characters
 	scanf("%9999s", dna);
+	if(verify(dna) == -1){
+		return -1
+	}
+	find(dna, threons);
 
 
 
@@ -22,4 +29,56 @@ int main() {
 
 	return 0;
 
+}
+
+int find(char dna[], char threons[]){
+	int found;
+	int i = 0;
+	int times = 0;
+	char temp [3];
+	while(dna[i] != '\0'){
+		int j;
+		for(j = 0; j < 3; j++){
+			temp[j] = dna [i];
+			i++;
+		}
+		int k;
+		int m = 0;
+		for(k = 0; k < 4; k ++){
+			if(threons[m] == dna[m]) {     // if first character of search string matches
+			// keep on searching
+				while(threons[m] !='\0') {
+					if(threons[m] == dna[m]){
+						m++;
+					}
+				}
+				if(m == 3){
+					found = j;
+					printf("%d\n", found);
+					times++;
+				}
+				else{
+					m = 0;
+					found = 0;
+        		}
+			}
+		}
+	}
+	if(times == 0){
+		//printf("%d\n", found);
+		return -1;
+	}
+}
+
+int verify(char dna[]){
+	int i = 0;
+	while(dna[i] != '\0'){
+		if(dna[i] != 'A' || dna[i] != 'C' || dna[i] != 'T' || dna[i] != 'G'){
+			return -1;
+		}
+		else{
+			return 1;
+		}
+
+	}
 }
