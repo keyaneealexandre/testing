b5e735cb0dd2dfebee93a8194426d10d0bf1a29f
keyaneealexandre <ka1974@nyu.edu>
2020-02-22 19:29:31 +0000

    problem3 solved

--- problem3.c
+++ problem3.c
@@ -3,8 +3,25 @@
 
 int main(){
 
-	/*
-	TODO: add your code here (or in the additional
-	functions that are called from here).
-	*/
+int num;
+   scanf("%d",&num);
+   int a = 0;
+   int b = 0;
+   int countBit = 0;//used to see how many 1 bits have been found and then further decides
+
+
+   int i;
+   for(i = 0; i < 31; i++) {
+       if(num & (1<<i)) {
+           if(countBit%2==0){
+           		a|=(1<<i);
+           }
+           else{
+           		b|=(1<<i);
+           }
+           countBit++;
+       }
+   }
+   printf("%d %d\n",a,b);
+   return 0;
 }
