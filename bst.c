731465929d5c891389841f6a94395de5d245ea7b
keyaneealexandre <ka1974@nyu.edu>
2020-02-22 06:05:12 +0000

    bst implemented

--- bst.c
+++ bst.c
@@ -3,77 +3,56 @@
 #include <string.h>
 #include "bst.h"
 
-typedef int (*comparer)(char, char);
+///typedef char (*comparer)(char, char);//used to compare two chars
 
 
-/*
-    create a new node
-*/
-bst_node *newNode(char data)
-{
-    bst_node *new_node = (bst_node*)malloc(sizeof(bst_node));
-    new_node->data = data;
-    new_node->left = NULL;
-    new_node->right = NULL;
-    return new_node;
+
+void add ( bst_node ** root, char *word ){
+    //strcpy(root, addRecurse((*root), word));
+    addRecurse((*root), word);
+    //root = *root;
+
 }
 
-
-void add ( bst_node ** root, char * word ) {
+bst_node *addRecurse(bst_node *root, char *word){
     if(root == NULL)
     {
-        root = create_node(data);
+        root = (bst_node*)malloc(sizeof(bst_node));
+        //strcpy(root->data, word);
+        root->data = word;
+        root->left = NULL;
+        root->right = NULL;
     }
-    addRecurse(root, compare, data);
-
-}
-
- void addRecurse(bst_node **node, comparer compare, char data){
-
-    bst_node ** current;
-
-
     /* Otherwise, recur down the tree */
-    if (compare(data,node->data) < 0){
-        if(node->left == NULL){
-            current = create_node(data);
-            node->left = current;
-        }
-        else{
-            node->left  = addRecurse(node->left, compare, data);
-        }
+    else if (strcmp(word,root->data) < 0){
+        //strcpy(root->left, add(root->left, word));
+        root->left  = addRecurse(root->left, word);
     }
     else{
-        if(node->right == NULL){
-            current = create_node(data);
-            node->right = current;
-        }
-        else{
-
-            node->right = addRecurse(node->right, compare, data);
-        }
+        //strcpy(root->right, add(root->right, word));
+        root->right = addRecurse(root->right, word);
     }
- }
-
+    return root;
+}
 
 void inorder ( bst_node * root ) {
     if (root != NULL)
     {
         inorder(root->left);
-        printf("%d \n", root->data);
+        printf("%s \n", root->data);
         inorder(root->right);
     }
 }
 
 
-char * remove(struct bst_node **root, int data) {
-  if (root == NULL) {
+bst_node * removeNode(bst_node *root, char *data) {
+ if (root == NULL) {
      return NULL;
   }
-  if (data < root->data) {  // data is in the left sub tree.
-      root->left = remove(root->left, data);
-  } else if (data > root->data) { // data is in the right sub tree.
-      root->right = remove(root->right, data);
+  if (strcmp(data,root->data) < 0) {  // data is in the left sub tree.
+      root->left = removeNode(root->left, data);
+  } else if (strcmp(data,root->data) > 0) { // data is in the right sub tree.
+      root->right = removeNode(root->right, data);
   } else {
      // case 1: no children
      if (root->left == NULL && root->right == NULL) {
@@ -82,27 +61,27 @@ char * remove(struct bst_node **root, int data) {
      }
      // case 2: one child (right)
      else if (root->left == NULL) {
-        struct bst_node *temp = root; // save current node as a backup
+        bst_node *temp = root; // save current node as a backup
         root = root->right;
         free(temp);
      }
      // case 3: one child (left)
      else if (root->right == NULL) {
-        struct Node *temp = root; // save current node as a backup
+        bst_node *temp = root; // save current node as a backup
         root = root->left;
         free(temp);
      }
      // case 4: two children
      else {
-        struct bst_node *temp = smallest(root->right); // find minimal value of right sub tree
+        bst_node *temp = smallest(root->right); // find minimal value of right sub tree
         root->data = temp->data; // duplicate the node
-        root->right = remove(root->right, temp->data); // delete the duplicate node
+        root->right = removeNode(root->right, temp->data); // delete the duplicate node
      }
   }
-  return root->data; // parent node can update reference
+  return root; // parent node can update reference
 }
 
-bst_node *smallest(bst_node **root){
+bst_node *smallest(bst_node *root){
     if(root == NULL){
         return root;
     }
@@ -113,15 +92,13 @@ bst_node *smallest(bst_node **root){
 }
 
 char * removeSmallest (bst_node ** root ){
-    if(root == NULL){
-        return root;
-    }
-    else{
-        return remove(smallest(root), root->data);
-    }
+    //root = smallest(*root);
+    //root = (*root);
+    (*root) = removeNode( smallest(*root), (*root)->data);
+    return (*root)->data;
 }
 
-bst_node *largest(bst_node **root){
+bst_node *largest(bst_node *root){
     if(root == NULL){
         return root;
     }
@@ -132,14 +109,10 @@ bst_node *largest(bst_node **root){
 }
 
 
-char * removeLargest (  bst_node ** root ){
+char * removeLargest ( bst_node ** root ){
 
-     if(root == NULL){
-        return root;
-    }
-    else{
-        return remove(largest(root), root->data);
-    }
+    (*root) = removeNode( largest(*root), (*root)->data);
+    return (*root)->data;
 }
 
 
