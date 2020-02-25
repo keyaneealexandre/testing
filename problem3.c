cea8b2c7a13f2a6e4b05b50e3ee01a59f32afa9f
keyaneealexandre <ka1974@nyu.edu>
2020-02-25 03:52:24 +0000

    problem3 completed

--- problem3.c
+++ problem3.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/*
+ * This program reads a value of n and produces two values a and b
+ * splitting a binary number n into two numbers a, b
+ * Author: Keyanee Alexandre
+ *
+ */
+
 
 int main(){
 
@@ -7,13 +14,13 @@ int num;
    scanf("%d",&num);
    int a = 0;
    int b = 0;
-   int countBit = 0;//used to see how many 1 bits have been found and then further decides
+   int countBit = 0;//used to see how many 1s have been found and then further decides
 
 
    int i;
    for(i = 0; i < 31; i++) {
-       if(num & (1<<i)) {
-           if(countBit%2==0){
+       if(num & (1<<i)) { //n&(1<<i) produces a non zero number when the ith bit of the number n is set else it will produce 0.
+           if(countBit%2==0){//
            		a|=(1<<i);
            }
            else{
